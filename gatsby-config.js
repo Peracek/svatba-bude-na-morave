9f360e9f6b56bbd2e81a89895230ac2c84670d7d
Pavel Peroutka <pavel.peroutka@ataccama.com>
2021-02-28 20:11:30 +0100

    fix img path

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/pages/`,
+        path: path.join(__dirname, `src`, `images`),
       },
     },
     `gatsby-plugin-sharp`,
