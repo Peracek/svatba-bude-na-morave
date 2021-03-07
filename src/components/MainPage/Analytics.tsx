2746318377982a970ef330f0ea40c6f764b644d4
Pavel Peroutka <pavel.peroutka@ataccama.com>
2021-03-07 22:05:01 +0100

    fix analytics

--- src/components/MainPage/Analytics.tsx
+++ src/components/MainPage/Analytics.tsx
@@ -1,9 +1,10 @@
 import { NoSsr } from '@material-ui/core'
 import React, { useEffect } from 'react'
+import firebase from 'firebase/app'
 
 const InitAnalytics = () => {
   useEffect(() => {
-    import('firebase/analytics')
+    import('firebase/analytics').then(() => firebase.analytics())
   }, [])
   return null
 }
